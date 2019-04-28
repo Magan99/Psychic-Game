f8a9d6e0cfee9f97d8752d9d425cc596966000ec
ahmed dahir <adahir2197@ahmeds-MacBook-Air.local>
2019-04-28 00:11:11 -0500

    Game one

---
+++ assests/javascript/game.js
@@ -0,0 +1,33 @@
+var wins = 0;
+var loses = 0;
+var guessesLeft = 10;
+var computerChoices =["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var yourGuessesSoFar = [];
+
+var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+console.log(computerGuess);
+
+document.onkeyup = function (event) {
+    console.log(event.key);
+    console.log("Hello", yourGuessesSoFar);
+    var userguess = event.key;
+
+    if (userguess === computerGuess) {
+        wins++;
+    }
+    if (userguess !== computerGuess) {
+        guessesLeft--;
+    }
+    if (guessesLeft === 0) {
+        loses++;
+    }
+    yourGuessesSoFar.push(userguess);
+
+
+    document.getElementById("wins").innerHTML = wins;
+    document.getElementById("loses").innerHTML = loses;
+    document.getElementById("guesses-left").innerHTML = guessesLeft;
+    document.getElementById("yourGuessesSoFar").innerHTML = yourGuessesSoFar;
+
+
+};
